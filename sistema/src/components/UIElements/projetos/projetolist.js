cad1d5bfda136126f6be52ed5935341ef7112b1f
joaovitor32 <joaovitormunizlopes@gmail.com>
2020-02-26 16:05:03 -0300

    Front - Sistema - Mudanças em Projeto List

--- sistema/src/components/UIElements/projetos/projetolist.js
+++ sistema/src/components/UIElements/projetos/projetolist.js
@@ -3,15 +3,32 @@ import React from 'react';
 import './projetolist.css';
 import Card from '../Card';
 
-const ProjetosList = props =>{
-    if(props.items.length===0){
+import ProjetoItem from './projetoitem';
+
+const ProjetosList = props => {
+    if (props.items.length === 0) {
         return (
-            
-                <h2>Nenhum projeto foi encontrado.</h2>
-            
+            <div className="error-message">
+                <Card>
+                    <h2>Não há nenhuma mensagem cadastrada.</h2>
+                </Card>
+            </div>
         )
-    }else{
-        
+    } else {
+        return (
+            <section className="flex">
+                {
+                    props.items.map(projeto =>
+                        <ProjetoItem
+                            key={props._id}
+                            nome={projeto.nome}
+                            link={projeto.link}
+                            tecnologia={projeto.tecnologia}
+                            imagem={projeto.imagem}
+                        />
+                    )}
+            </section>
+        )
     }
 };
 
