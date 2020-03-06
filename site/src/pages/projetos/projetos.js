c9cb7cae7100736102f28f5aa914696b7dcdfa03
joaovitor32 <joaovitormunizlopes@gmail.com>
2020-03-06 17:16:01 -0300

    Mudanças page projetos - Site

--- site/src/pages/projetos/projetos.js
+++ site/src/pages/projetos/projetos.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-const Projetos = props=>{
-    return (<p>
-       projetos
-    </p>)
+import './projetos.css';
+
+import ProjetosComponent from '../../components/projetos/projetoscomponent';
+import Footer from '../../components/footer/footer';
+
+import LoadingSpinner from '../../components/LoadingSpinner/LoadingSpinner'
+
+const Projetos = props => {
+
+    const [load, setLoad] = useState(true);
+
+    useEffect(() => {
+        setLoad(false);
+    }, [setLoad])
+
+    return (
+
+        <React.Fragment>
+            {load && <LoadingSpinner />}
+            {!load &&
+                <React.Fragment>
+                    <ProjetosComponent />
+                    <Footer />
+                </React.Fragment>
+            }
+        </React.Fragment>
+
+    )
 }
 
 export default Projetos;
