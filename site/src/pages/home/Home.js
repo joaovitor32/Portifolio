07ab554cc5d00d8ba862d238873fd11da6bff3d6
joaovitor32 <joaovitormunizlopes@gmail.com>
2020-03-06 13:38:52 -0300

    Mudanças em page - Home

--- site/src/pages/home/Home.js
+++ site/src/pages/home/Home.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-import Perfil  from '../../components/home/perfil'
+import Perfil from '../../components/home/perfil'
+import Descricao from '../../components/home/descricao';
+import Cursos from '../../components/home/cursos/curso';
+import Footer from '../../components/footer/footer';
 
-const Home = props=>{
+import './Home.css'
+
+const Home = props => {
+
+    const [load, setLoad] = useState(false);
+
+    useEffect(() => {
+        setLoad(true);
+    }, [setLoad])
+
     return (
-       <React.Fragment>
-            <Perfil></Perfil>
-       </React.Fragment>
+        <div className={load ? "fadeIn" : "fadeOut"}>
+            <div className="background">
+                <Perfil />
+            </div>
+            <Descricao />
+            <Cursos />
+            <Footer />
+        </div>
     )
 }
 
