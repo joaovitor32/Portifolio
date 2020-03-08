9f9d35c2836b1c7ab493024b0bc133db31864636
joaovitor32 <joaovitormunizlopes@gmail.com>
2020-03-08 18:49:00 -0300

    Mudanças no backend

--- backend/controller/message-controller.js
+++ backend/controller/message-controller.js
@@ -7,10 +7,12 @@ const mongoose=require('mongoose');
 
 const createMessage= async(req,res,next)=>{
     const errors=validationResult(req);
-    if(!errors.isEmpty()){
+    /*if(!errors.isEmpty()){
         return next(new HttpError('Algum dado do formulário está incorreto',422))
-    }
+    }*/
+
     const {name,email,mensagem}=req.body;
+
     const createdMessage= new Mensagem({
         name,
         email,
@@ -22,7 +24,7 @@ const createMessage= async(req,res,next)=>{
         const error= new HttpError('Mensagem não cadastrada tente de novo',500);
         return next(error);
     }
-    res.status(201).json({place:createdMessage});
+    res.status(201).json({message:createdMessage});
 
 } 
 
